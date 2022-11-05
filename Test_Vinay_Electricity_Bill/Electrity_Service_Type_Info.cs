cc7fb5e04e9ce08b4f052349a59510c109e68b3f
Rintu <Rintu@VINDUJA>
2022-11-05 15:47:47 +0530

    Add project files.

---
+++ Test_Vinay_Electricity_Bill/Electrity_Service_Type_Info.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Vinay_Electricity_Bill
+{
+    public class Electrity_Service_Type_Info
+    {
+        public int Units { get; set; }
+        public int MeterNumber { get; set; }
+        public string? CustomerName { get; set; }
+        public string? ServiceType { get; set; }
+
+        double bill;
+
+        public void ServiceTypeBill()
+        {
+            if (ServiceType == "d" || ServiceType == "D")
+            {
+                if (Units < 100)
+                {
+                    bill = Units * 0.35;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 100 && Units <= 200)
+                {
+                    bill = Units * 0.85;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 200 && Units <= 400)
+                {
+                    bill = Units * 1.15;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 400 && Units <= 700)
+                {
+                    bill = Units * 1.15;
+                    Console.WriteLine(bill);
+                }
+                else
+                {
+                    bill = Units * 2.80;
+                    Console.WriteLine(bill);
+                }
+
+            }
+            else if(ServiceType == "c" || ServiceType == "C")
+            {
+
+                if (Units < 100)
+                {
+                    bill = Units * 0.90;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 100 && Units <= 200)
+                {
+                    bill = Units * 1.35;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 200 && Units <= 400)
+                {
+                    bill = Units * 1.95;
+                    Console.WriteLine(bill);
+                }
+                else if (Units > 400 && Units <= 700)
+                {
+                    bill = Units * 2.45;
+                    Console.WriteLine(bill);
+                }
+                else
+                {
+                    bill = Units * 3.25;
+                    Console.WriteLine(bill);
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Test_Vinay_Electricity_Bill/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test_Vinay_Electricity_Bill;
+
+Console.WriteLine("Hello, World!");
+
+
+Electrity_Service_Type_Info objElectricityType = new Electrity_Service_Type_Info();
+
+Console.WriteLine("Enter Meter Number ");
+objElectricityType.MeterNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Customer Name");
+objElectricityType.CustomerName = Console.ReadLine();
+
+Console.WriteLine("Enter Units");
+objElectricityType.Units = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Your Service Type is Domestic Enter 'd' or 'D' \n" +
+    "Your Service Type is Commercial Enter 'c' or 'C'");
+objElectricityType.ServiceType = Console.ReadLine();
+
+objElectricityType.ServiceTypeBill();
